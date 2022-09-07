723bc2e3cf07def73673d188a0ef6c74131f7588
Matt Plunkett <marketlinkresearch@gmail.com>
2022-09-07 17:07:42 +0100

    initial commit, also finished project

---
+++ index.js
@@ -0,0 +1,42 @@
+const myEmojis = ['👨‍💻', '⛷', '📖', '🏋🏻‍♂️'];
+const emojiContainer = document.getElementById('emoji-container');
+const emojiInput = document.getElementById('emoji-input');
+const pushBtn = document.getElementById('push-btn');
+const unshiftBtn = document.getElementById('unshift-btn');
+const shiftBtn = document.getElementById('shift-btn');
+const popBtn = document.getElementById('pop-btn');
+
+function renderEmojis() {
+  emojiContainer.innerHTML = '';
+  for (let i = 0; i < myEmojis.length; i++) {
+    const emoji = document.createElement('span');
+    emoji.textContent = myEmojis[i];
+    emojiContainer.append(emoji);
+    emojiInput.value = '';
+  }
+}
+renderEmojis();
+
+pushBtn.addEventListener('click', function () {
+  if (emojiInput.value) {
+    myEmojis.push(emojiInput.value);
+    renderEmojis();
+  }
+});
+
+unshiftBtn.addEventListener('click', function () {
+  if (emojiInput.value) {
+    myEmojis.unshift(emojiInput.value);
+    renderEmojis();
+  }
+});
+
+shiftBtn.addEventListener('click', function () {
+  myEmojis.shift();
+  renderEmojis();
+});
+
+popBtn.addEventListener('click', function () {
+  myEmojis.pop();
+  renderEmojis();
+});
